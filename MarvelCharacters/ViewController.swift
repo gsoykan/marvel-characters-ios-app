d5b89d92032cdffa4df5358045a8134cc963b5c7
gsoykan <gurkan@naylalabs.com>
2020-06-08 22:32:07 +0300

    Initial Commit

---
+++ MarvelCharacters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by gurkan soykan on 8.06.2020.
+//  Copyright © 2020 gurkan soykan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarvelCharactersTests/MarvelCharactersTests.swift
@@ -0,0 +1,34 @@
+//
+//  MarvelCharactersTests.swift
+//  MarvelCharactersTests
+//
+//  Created by gurkan soykan on 8.06.2020.
+//  Copyright © 2020 gurkan soykan. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelCharacters
+
+class MarvelCharactersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
